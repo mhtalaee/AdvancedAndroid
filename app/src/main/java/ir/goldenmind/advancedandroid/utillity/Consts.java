99d0678c115711cfb444c0d44a2b74b11cd720dc
mhtalaee <mhtalaee@gmail.com>
2019-04-21 21:40:36 +0430

    Retrofit

--- app/src/main/java/ir/goldenmind/advancedandroid/utillity/Consts.java
+++
@@ -1,5 +0,0 @@
-package ir.goldenmind.advancedandroid.utillity;
-
-public class Consts {
-    public static final String IMDBWebServiceBaseAddress = "http://www.omdbapi.com";
-}
---
+++ app/src/main/java/ir/goldenmind/advancedandroid/utillity/Consts.kt
@@ -0,0 +1,5 @@
+package ir.goldenmind.advancedandroid.utillity
+
+object Consts {
+    val IMDBWebServiceBaseAddress = "http://www.omdbapi.com"
+}

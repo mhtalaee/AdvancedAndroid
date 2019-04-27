63da981e41b266f566ac424790bf531c8642e96c
Talaee <mhtalaee@gmail.com>
2019-04-27 15:49:03 +0430

    Check Memory Leak

--- app/src/main/java/ir/goldenmind/advancedandroid/javaversion/BaseApplication.java
+++ app/src/main/java/ir/goldenmind/advancedandroid/javaversion/BaseApplication.java
@@ -1,12 +1,25 @@
 package ir.goldenmind.advancedandroid.javaversion;
 
 import android.app.Application;
-import android.widget.Toast;
+import android.content.Context;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 public class BaseApplication extends Application {
 
+    RefWatcher refwatcher;
+
     @Override
-    public void onTerminate() {
-        super.onTerminate();
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        refwatcher = LeakCanary.install(this);
     }
 }
---
+++ app/src/test/java/ir/goldenmind/advancedandroid/MemoryLeak.java
@@ -0,0 +1,23 @@
+package ir.goldenmind.advancedandroid;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MemoryLeak {
+
+    private Random random = new Random();
+    public static final ArrayList<Double> list = new ArrayList<Double>(1000000);
+
+    @Test
+    public void givenStaticField_whenLotsOfOperations_thenMemoryLeak() throws InterruptedException {
+        for (int i = 0; i < 1000000; i++) {
+            list.add(random.nextDouble());
+        }
+
+        System.gc();
+        Thread.sleep(10000); // to allow GC do its job
+    }
+
+}

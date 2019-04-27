ab8be767c5ad05d4969a2d6e42c0e1be1772708a
Talaee <mhtalaee@gmail.com>
2019-04-27 15:16:51 +0430

    Add BAseApplication class

---
+++ app/src/main/java/ir/goldenmind/advancedandroid/javaversion/BaseApplication.java
@@ -0,0 +1,12 @@
+package ir.goldenmind.advancedandroid.javaversion;
+
+import android.app.Application;
+import android.widget.Toast;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}

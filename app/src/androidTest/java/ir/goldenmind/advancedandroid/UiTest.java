83c21803eed7cf5d09701a6ac1f97f1201a5dfb1
Talaee <mhtalaee@gmail.com>
2019-05-01 12:10:55 +0430

    delete image

--- app/src/androidTest/java/ir/goldenmind/advancedandroid/UiTest.java
+++ app/src/androidTest/java/ir/goldenmind/advancedandroid/UiTest.java
@@ -12,14 +12,11 @@ import androidx.test.runner.AndroidJUnit4;
 
 import ir.goldenmind.advancedandroid.javaversion.activities.TestUiActivity;
 
-import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
